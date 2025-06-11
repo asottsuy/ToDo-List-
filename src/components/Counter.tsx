14ae1534963f61faf06635d25866172b9345f7a5
Felipe Tatsuya Aso <asotatsuya0304@gmail.com>
2025-06-11 18:05:45 -0300

    Finalizando o layout do projeto

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,6 +1,12 @@
 import styles from './Counter.module.css';
 
+import { useState } from 'react';
+
 export function Counter(){
+
+    const [counter, setCounter] = useState(0);
+
+
     return(
         
         <span className={styles.circle} id='counter'>0</span>
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -6,11 +6,16 @@ import { TrashIcon } from '@phosphor-icons/react';
 export function Task(){
     return(
         <div className={styles.taskContent}>
-            <div className={styles.imgContent}>
-                <input className={styles.taskCheckbox}></input>
-            </div>
 
-            <p className={styles.taskText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolorum quidem dolores obcaecati laudantium ratione, nulla aliquid, optio, suscipit </p>
+            <label className={styles.imgContent}>
+                <input 
+                    className={styles.taskCheckbox} 
+                    type='checkbox'>
+                </input>
+
+                <p className={styles.taskText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolorum quidem dolores obcaecati laudantium ratione, nulla aliquid, optio, suscipit </p>
+            </label>
+
 
             <button>
                 <TrashIcon/>
